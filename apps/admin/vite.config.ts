86d49c5f5af21d3e8ec73b53f99fb1f69ff392dc
caesarKom <cezarykomorowski@gmail.com>
2026-09-12 12:19:29 +0200

    fix port

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -5,6 +5,10 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/admin/',
+  server: {
+    port: 5002,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
